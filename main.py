77657d8e3cd5e4621832eead3dae089580a14746
LorenzoFerri <lferri@ogs.it>
2024-08-08 14:30:08 +0000

    script funzionante v1

--- main.py
+++ main.py
@@ -1,36 +1,56 @@
-# importazione delle librerie
-import csv
+
+# %% importazione delle librerie
+import os
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from datetime import datetime 
-import calendar
 import pandas as pd
+import sys
 
-# variabili da settare 
+# %% variabili da settare 
+
 minutes=50;
-startUrlString=["https://nodc.ogs.it/erddap/tabledap/CURRISO_PR.htmlTable?time%2Clatitude%2Clongitude%2Cdepth%2CVCSP%2CVCSP_QC%2CEWCT%2CEWCT_QC%2CNSCT%2CNSCT_QC&time%3E=",
-                    "https://nodc.ogs.it/erddap/tabledap/CURRISO_TS.htmlTable?time%2Clatitude%2Clongitude%2CPRES%2CPRES_QC%2CTEMP%2CTEMP_QC%2CRVFL%2CRVFL_QC&time%3E="];
+startUrlString=["https://nodc.ogs.it/erddap/tabledap/CURRISO_PR.htmlTable?time%2Clatitude%2Clongitude%2Cdepth%2CVCSP%2CVCSP_QC%2CEWCT%2CEWCT_QC%2CNSCT%2CNSCT_QC&time%3E",
+                    "https://nodc.ogs.it/erddap/tabledap/CURRISO_TS.htmlTable?time%2Clatitude%2Clongitude%2CPRES%2CPRES_QC%2CTEMP%2CTEMP_QC%2CRVFL%2CRVFL_QC&time%3E"];
 endUrlString=["&orderBy(%22time%2Cdepth%22)","&orderBy(%22time%22)"];
-newUrlString=[]
 
+# %% definizione variabili globali
+
+newUrlString=[]
 dfList=[]
+dfProfile=[]
 finalDf=pd.DataFrame()
 
-# definizione del datetime corretto
-currentDateTime = datetime.now() 
-print(currentDateTime)
+# %% controllo dateList per definizione datetime
+
+tempTimeObj=open("dateList.txt",'r')
+tempTime=tempTimeObj.read()
+tempTimeObj.close()
+newDateTime=datetime.strptime(tempTime, "%d-%m-%Y %H:%M:%S")
 
 
-dateTimeSeconds=calendar.timegm(currentDateTime.timetuple())
-dateTimeSeconds=dateTimeSeconds-minutes*60
-newDateTime=datetime.fromtimestamp(dateTimeSeconds)
-print(newDateTime)
+# tempStringObj=open("temp.csv",'w')
+# tempStringObj.write(tempStringMod)
+# tempStringObj.close()
 
-# lettura dei dati web attraverso BeautifulSoup
-for urlIdx in range(len(startUrlString)):
+# %% definizione del datetime corretto
+
+# currentDateTime = datetime.now() 
+# print(currentDateTime)
+
+# dateTimeSeconds=calendar.timegm(currentDateTime.timetuple())
+# dateTimeSeconds=dateTimeSeconds-minutes*60
+# newDateTime=datetime.fromtimestamp(dateTimeSeconds)
+# print(newDateTime)
+
+# %% lettura dei dati web attraverso BeautifulSoup
+try:
     
-    #composizione della stringa per la query al sito 
-    newUrlString.append(startUrlString[urlIdx]+ \
+    for urlIdx in range(len(startUrlString)):
+    
+    #-------------------------composizione della stringa per la query al sito 
+    
+        newUrlString.append(startUrlString[urlIdx]+ \
                     newDateTime.strftime("%Y-%m-%dT")+ \
                     newDateTime.strftime("%H")+ \
                     "%3A"+ \
@@ -38,17 +58,21 @@ for urlIdx in range(len(startUrlString)):
                     "%3A"+ \
                     newDateTime.strftime("%SZ") +\
                     endUrlString[urlIdx] );
-    print(newUrlString[urlIdx])
+        print(newUrlString[urlIdx])
 
-    #creazione oggetto di beatifulsoup e relativo dataframe pandas
-    html = urlopen(newUrlString[urlIdx]);
-    soup= BeautifulSoup(html.read(),'html.parser');
-    htmlTable=soup.find_all("table",{"class":"erd commonBGColor nowrap"});
-    df=pd.read_html(str(htmlTable))[0];
-    dfList.append(df);
+    # -- creazione oggetto di beatifulsoup e relativo dataframe pandas
     
+        html = urlopen(newUrlString[urlIdx]);
+        soup= BeautifulSoup(html.read(),'html.parser');
+        htmlTable=soup.find_all("table",{"class":"erd commonBGColor nowrap"});
+        df=pd.read_html(str(htmlTable))[0];
+        dfList.append(df);
+
+except:  
+    sys.exit()
     
-# elaborazione del dataframe finale 
+# %% creazione finalDf con data e ora
+
 finalDf['tempDate'] = pd.to_datetime(dfList[1]['time','UTC'], format='%Y-%m-%dT%H:%M:%SZ');  
 finalDf['date'] = finalDf['tempDate'].dt.strftime('%d/%m/%Y');
 finalDf['time'] = finalDf['tempDate'].dt.strftime('%H:%M:%S');
@@ -56,39 +80,68 @@ finalDf=finalDf.drop('tempDate',axis='columns');
 finalDf['latitude'] = dfList[1]['latitude','degrees_north']
 finalDf['longitude'] = dfList[1]['longitude','degrees_east']
 
+# %% elaborazione dataframe con profili  
 
-for rowIdx in range(len(dfList[0])):
+dfList[0]=dfList[0].drop(['latitude','longitude','VCSP_QC','EWCT_QC','NSCT_QC'],axis='columns');
+dfList[0].to_csv("temp.csv",index=False,header=False)
+dfList[0] = pd.read_csv('temp.csv',sep=",", header=None)
+
+profileColoumnName=["time","depth", "VCSP", "EWCT", "NSCT"]
+dfList[0]= dfList[0].set_axis(profileColoumnName, axis=1)
+
+gr=dfList[0].groupby('time')
+keys=list(gr.groups.keys())
+finalProfileDf=pd.DataFrame()
+
+
+# %%
+
+for keyIdx in range (len(keys)):  
+    dfProfile.append(gr.get_group(keys[keyIdx]))
+    dfProfile[keyIdx]=dfProfile[keyIdx].drop('time',axis='columns');
+    dfProfile[keyIdx].insert(0,"#",list(range(1,len(dfProfile[keyIdx])+1)))
+    dfProfile[keyIdx].to_csv("temp.csv",index=False,header=False)
+    tempStringObj=open("temp.csv",'r')
+    tempString=tempStringObj.read()
+    tempStringObj.close()
+    tempStringMod = tempString.replace("\n", ",")
+    tempStringMod=tempStringMod[:-1]
+    tempStringObj=open("temp.csv",'w')
+    tempStringObj.write(tempStringMod)
+    tempStringObj.close()
+    dfProfile[keyIdx]=pd.read_csv("temp.csv",header=None)
+    finalProfileDf=pd.concat([finalProfileDf,dfProfile[keyIdx]],ignore_index=True)
+
+#%% elaborazione dataframe con timeseries
+dfList[1]=dfList[1].drop(['time','latitude','longitude','PRES_QC','TEMP_QC','RVFL_QC'],axis='columns');
+dfList[1].to_csv("temp.csv",index=False,header=False)
+dfList[1] = pd.read_csv('temp.csv',sep=",", header=None) 
+
+finalDf=pd.concat([finalDf,dfList[1], finalProfileDf],axis=1)    
     
-    # prova esportare csv 
-    
-    # da qui è da controllare 
-    tempDf=dfList[0].iloc[2:3,3:9]
-    ccc=pd.concat([finalDf,tempDf],axis=0)
-    finalDf=finalDf.join(tempDf)
-    horizontal_concat = pd.concat([finalDf, tempDf], axis=1)
-    vvv=[finalDf,tempDf]
-    
-print(finalDf)
+# %% set coloumn name 
 
+startColoumnName=["date","time", "latitude", "longitude"]  
+profileColoumnName=[]
+for rowIdx in range(int(len(finalProfileDf.columns)/5)):
+    profileColoumnName=profileColoumnName+["PROFILE #","DEPTH", "VCSP", "EWCT", "NSCT"]
+tsColoumnName=["PRES","TEMP","RVFL"]
+finalColoumnName=startColoumnName+tsColoumnName+profileColoumnName
+finalDf = finalDf.set_axis(finalColoumnName, axis=1)   
 
-# salvataggio in formato csv 
-currentDateTimeString=currentDateTime.strftime("%Y-%d-%mT_%H-%M-%S")
+    #%% scrittura del dataframe finale su file
 
-csvFileName='ERDDAP_Curriso_Profile_'+currentDateTimeString+'.csv'
-with open(csvFileName, 'w', newline='') as file: 
-    writer = csv.writer(file)
-    writer.writerow(soup)
+currentDateTimeString=datetime.now().strftime("%m-%d-%Y_%H-%M-%S")    
+csvFileName='csv/ERDDAP_CurrIso_'+currentDateTimeString+'.csv'  
+  
+finalDf.to_csv(csvFileName,index=False)    
 
+os.remove("temp.csv")
 
-
-
-
-
-
-
-
-
-
-
-
+endTime=dfList[0].time[len(dfList[0])-1]
+endTime=datetime.strptime(endTime, '%Y-%m-%dT%H:%M:%SZ')
+timeString=datetime.strftime(endTime, '%d-%m-%Y %H:%M:%S')
+tempTimeObj=open("dateList.txt",'w')
+tempTimeObj.write(timeString)
+tempTimeObj.close() 
 
